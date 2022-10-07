dc215c80eb07646d481cbb3869db6eba2c262683
Валери Ковалева <kovaleva.valeri71@gmail.com>
2022-10-07 13:16:29 +0300

    Добавление get запросов в маршрутизации с параметрами

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const { response } = require("express");
 const express = require("express");
  
 const app = express();
@@ -6,6 +7,8 @@ const app = express();
 объект - результат парсинга будет представлять набор пар ключ-значение,
 а каждое значение может быть представлено в виде строки или массив*/
 const urlencodedParser = express.urlencoded({extended: false});
+
+var permiss = false;
 
 //start
 app.get("/", function(_, response){
@@ -18,40 +21,53 @@ app.post("/", urlencodedParser, function (request, response) {
     console.log(request.body);
     //Не работает вывод результата на html
     //https://metanit.com/web/nodejs/4.5.php
-    if (request.body.userAge > 17) response.redirect("index"); else response.send("${request.body.userName} - ${request.body.userAge}<br><h1>Доступ закрыт, прости<h1>");
+    if (request.body.userAge > 17) {
+        permiss = true;
+        response.redirect("index"); 
+    }
+    else response.send('${userName} - ${userAge}',
+     request.body.userName, request.body.userAge);
 });
+
+//file-css
+app.get("/css/navbar.css", (_, response) => response.sendFile(__dirname +"/css/navbar.css"))
 
 //index
-app.get("/index", function(_, response){
-    //отправка файла html по адресу /
-    response.sendFile(__dirname+"/index.html");
-});
+app.get("/index", (_, response) => 
+    //отправка файла html по адресу /index
+    response.sendFile(__dirname+"/index.html"));
 
 //room
+// определение Router для room
+const roomRouter = express.Router();
+
 //статичные файлы в папке room
 app.use("/room", express.static(__dirname+'/room'));
 
-app.get("/room", function(_, response){
-     
-    response.send("<h1>Комната</h1>");
-});
+app.get("/room", (_, response) => response.sendFile(__dirname+"/room/room.html"));
 
+//bedroom
+//books
+app.get("/room/bedroom/books/:bookId", function (request, response) {
+    response.send("bookId: " + request.params["bookId"])
+  });
+
+app.get("/room/bedroom/books/:bookId.:ext", (req, res) =>{
+    let bookId = req.params["bookId"];
+    let ext = req.params["ext"];
+    res.send('Запрошенный файл: ${bookId}.${ext}', bookId, ext)
+})
+//сопоставление роутер с конечной точкой "/room"
+//app.use("/room", roomRouter);
 //bathroom
-app.get("/bathroom", function(_, response){
-     
-    response.send("<h1>Ванная</h1>");
-});
+app.get("/bathroom", (_, response) => response.send("<h1>Ванная</h1>"));
 
 //about
 //отправка файла about.html
-app.get("/about", function(_,response){
-    response.sendFile(__dirname+"/about.html");
-});
+app.get("/about", (_,response) => response.sendFile(__dirname+"/about.html"));
 
 //*
 //обработка статусного кода 404
-app.get("*", function(_,response){
-    response.status(404).send('Ресур не найден');
-});
+app.get("*", (_,response) => response.status(404).send('Ресур не найден'));
 
 app.listen(3000, ()=>console.log("Сервер запущен..."));
